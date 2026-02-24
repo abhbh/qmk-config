86cb56c816495c6ed2b3fceeac5e30f48ae0e04e
Abhinav <abhnv@abhnv.com>
2026-02-24 05:48:47 +0530

    Sofle keymap

--- keyboards/sofle_choc/keymaps/abhbh/keymap.c
+++ keyboards/sofle_choc/keymaps/abhbh/keymap.c
@@ -1,56 +1,69 @@
 #include QMK_KEYBOARD_H
 
 
+enum layer_names {
+    _BASE,
+    _EXT,
+    _SYM,
+    _FUNC,
+    _WKNM,
+    _LGCY
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // -----> Base layer <-----
-    [0] = LAYOUT(
-         KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_LGUI, /* |-----| */ KC_RCTL, KC_J, KC_L,    KC_U,   KC_Y, KC_QUOT,      KC_BSPC,
-         KC_ESC, KC_A, KC_R, KC_S, KC_T, KC_G,  OSL(5), /* |-----| */ KC_LALT, KC_M, KC_N,    KC_E,   KC_I,    KC_O,       KC_ENT,
-        OS_RSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, /* //////-------------\\\\\\ */ KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, LT(5,KC_APP),
-                               OS_LCTL, OSL(1), KC_SPC, /* /-----\ */ OS_LSFT, OSL(2), OS_LGUI),
+    [_BASE] = LAYOUT(
+        KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5, /* |-------------| */  KC_6,  KC_7,    KC_8,   KC_9,    KC_0, KC_BSPC,
+        KC_TAB,  KC_Q,  KC_W,  KC_F,  KC_P,  KC_B, /* |-------------| */  KC_J,  KC_L,    KC_U,   KC_Y, KC_QUOT, KC_BSPC,
+        KC_ESC,  KC_A,  KC_R,  KC_S,  KC_T,  KC_G, /* |-------------| */  KC_M,  KC_N,    KC_E,   KC_I,    KC_O,  KC_ENT,
+        OS_RSFT, KC_Z,  KC_X,  KC_C,  KC_D,  KC_V, KC_MUTE /**/ KC_MUTE,  KC_K,  KC_H, KC_COMM, KC_DOT, KC_SLSH, LT(4,KC_APP),
+             OSL(4), OS_LALT, OS_LCTL, OSL(1), KC_SPC, /* /----\ */ OS_LSFT, OSL(2), OS_LGUI, OS_RCTL, OSL(4)),
 
 // -----> Extend layer <-----
-    [1] = LAYOUT(
-        KC_TRNS, KC_HOME,  C(KC_LEFT),   KC_UP, C(KC_RGHT), KC_PGUP, KC_VOLU, /* |------| */   KC_NO,  KC_DEL, KC_BSPC, C(KC_BSPC),  OS_MEH, KC_CAPS, KC_TRNS,
-        KC_TRNS,  KC_END,     KC_LEFT, KC_DOWN,    KC_RGHT, KC_PGDN, KC_VOLD, /* |------| */   KC_NO, KC_LGUI, OS_LCTL,    OS_LALT, OS_LGUI, OS_RSFT, KC_TRNS,
-        KC_TRNS,  KC_ESC, LCS(KC_TAB),  KC_TAB,  C(KC_TAB), QK_AREP, /* /////----------------\\\\\ */  QK_REP,  OS_LCS,     KC_ENT,  KC_APP, KC_PSCR,   TG(5),
-                                                    KC_TRNS, KC_TRNS, KC_TRNS, /* /------\ */   MO(3), MO(4), KC_TRNS),
+    [_EXT] = LAYOUT(
+        _______,   KC_F1,       KC_F2,   KC_F3,      KC_F4,   KC_F5, /* |-------------| */   KC_F6,   KC_F7,      KC_F8,   KC_F9,    KC_F10,  KC_F11,
+        _______, KC_HOME,  C(KC_LEFT),   KC_UP, C(KC_RGHT), KC_PGUP, /* |-------------| */  KC_DEL, KC_BSPC, C(KC_BSPC),  OS_MEH,   KC_CAPS,  KC_F12,
+        _______,  KC_END,     KC_LEFT, KC_DOWN,    KC_RGHT, KC_PGDN, /* |-------------| */ KC_LGUI, OS_LCTL,    OS_LALT, OS_LGUI,   OS_RSFT, _______,
+        _______,  KC_ESC, LCS(KC_TAB),  KC_TAB,  C(KC_TAB), QK_AREP, KC_MPLY /**/ KC_MPLY,  QK_REP,  OS_LCS, KC_COMM, KC_DOT, KC_SLSH, _______,
+                              OSL(4), OS_LALT, OS_LCTL, _______, KC_SPC, /* /----\ */ OS_LSFT, OSL(3), OS_LGUI, OS_RCTL, OSL(5)),
+
 
 // -----> Symbols layer <-----
-    [2] = LAYOUT(
-        KC_TRNS,  KC_GRV,  KC_EQL, KC_HASH, KC_EXLM, KC_PERC, RALT(KC_4), /* |------| */  KC_NO, KC_ASTR, KC_AMPR, KC_UNDS, KC_COLN, KC_SCLN, KC_TRNS,
-        KC_TRNS, KC_TILD, KC_MINS, KC_LPRN, KC_LCBR, KC_LBRC,      KC_NO, /* |------| */  KC_NO,   KC_AT, OS_LCTL, OS_LALT, OS_LGUI, OS_RSFT, KC_TRNS,
-        KC_TRNS,  KC_DLR, KC_PLUS, KC_RPRN, KC_RCBR, KC_RBRC,     /* /////--------------\\\\\ */ KC_CIRC, KC_PIPE,   KC_LT,   KC_GT, KC_BSLS,   TG(5),
-                                                   KC_TRNS, MO(4), MO(3), /* /------\ */ KC_ENT, KC_TRNS, KC_TRNS),
+    [_SYM] = LAYOUT(
+        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, /* |-------------| */    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+        _______,  KC_GRV,  KC_EQL, KC_HASH, KC_EXLM, KC_PERC, /* |-------------| */ KC_ASTR, KC_AMPR, KC_UNDS, KC_COLN, KC_SCLN, _______,
+        _______, KC_TILD, KC_MINS, KC_LPRN, KC_LCBR, KC_LBRC, /* |-------------| */   KC_AT, OS_LCTL, OS_LALT, OS_LGUI, OS_RSFT, _______,
+      LCA(KC_4),  KC_DLR, KC_PLUS, KC_RPRN, KC_RCBR, KC_RBRC, KC_MUTE /**/ KC_MUTE, KC_CIRC, KC_PIPE,   KC_LT,   KC_GT, KC_BSLS, _______,
+                        OSL(4), OS_LALT, OS_LCTL, OSL(3), KC_SPC, /* /----\ */ OS_LSFT, _______, OS_LGUI, OS_RCTL, OSL(5)),
+
 
 // -----> Adjust and Function layer <-----
-    [3] = LAYOUT(
-        QK_BOOT,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, OS_HYPR, KC_NO, /* |-----| */ KC_NO,  KC_F12, KC_F7, KC_F8, KC_F9,  KC_INS, KC_DEL,
-          KC_NO, OS_RSFT, OS_LGUI, OS_LALT, OS_LCTL,  OS_MEH, KC_NO, /* |-----| */ KC_NO,  KC_F11, KC_F4, KC_F5, KC_F6, KC_PAUS,  KC_NO,
-          KC_NO, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT,   TG(6), /* ///---------------\\\ */  KC_F10, KC_F1, KC_F2, KC_F3, KC_SCRL,  KC_NO,
-                                              KC_NO, KC_TRNS, KC_NO, /* /-----\ */ KC_NO, KC_TRNS, KC_NO),
+    [_FUNC] = LAYOUT(
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /* |-------------| */ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        XXXXXXX,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, OS_HYPR, /* |-------------| */  KC_F12,   KC_F7,   KC_F8,   KC_F9,  KC_INS, XXXXXXX,
+        XXXXXXX, OS_RSFT, OS_LGUI, OS_LALT, OS_LCTL,  OS_MEH, /* |-------------| */  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_PAUS, XXXXXXX,
+        XXXXXXX, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT,   TG(5), KC_MUTE /**/ KC_MUTE,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_SCRL, XXXXXXX,
+                     XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, /* /----\ */ XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX),
 
-// -----> Numbers layer <-----
-    [4] = LAYOUT(
-        KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_NO, /* |-----| */ KC_NO,    KC_6, KC_7, KC_8, KC_9,    KC_0,  KC_TRNS,
-        KC_TRNS, OS_RSFT, OS_LGUI, OS_LALT, OS_LCTL,  OS_MEH, KC_NO, /* |-----| */ KC_NO,    KC_0, KC_4, KC_5, KC_6, KC_COMM,  KC_TRNS,
-        KC_TRNS,  KC_ESC, KC_LEFT,  KC_TAB, KC_RGHT, QK_AREP, /* ///---------------\\\ */  QK_REP, KC_1, KC_2, KC_3,  KC_DOT,   KC_APP,
-                                          KC_TRNS, KC_TRNS, KC_TRNS, /* /-----\ */ KC_TRNS, KC_0, KC_TRNS),
 
 // -----> Workspace Management and Numpad layer <-----
-    [5] = LAYOUT(
-        KC_TRNS, LCG(KC_1),    LCG(KC_2), LCG(KC_3),    LCG(KC_4), LCG(KC_5), KC_TRNS, /* |-----| */  KC_NUM, KC_PMNS,  KC_P7, KC_P8, KC_P9,   KC_P0, KC_TRNS,
-        KC_TRNS, LCG(KC_6),    LCG(KC_7), LCG(KC_8),    LCG(KC_9), LCG(KC_0), KC_TRNS, /* |-----| */ KC_TRNS, KC_PAST,  KC_P4, KC_P5, KC_P6, KC_PPLS, KC_TRNS,
-        KC_TRNS,    KC_ESC, LSG(KC_LEFT),    KC_TAB, LSG(KC_RGHT), LCG(KC_C), /* /////---------------\\\\\ */ KC_PSLS,  KC_P1, KC_P2, KC_P3, KC_PDOT,   TG(5),
-                                                              KC_TRNS, TG(5), KC_TRNS, /* /-----\ */ KC_PENT, KC_P0, KC_ESC),
+    [_WKNM] = LAYOUT(
+        _______,   XXXXXXX,      XXXXXXX,   XXXXXXX,      XXXXXXX,   XXXXXXX, /* |-------------| */  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, LCG(KC_1),    LCG(KC_2), LCG(KC_3),    LCG(KC_4), LCG(KC_5), /* |-------------| */  KC_PMNS,   KC_P7,   KC_P8,   KC_P9,   KC_P0, _______,
+        _______, LCG(KC_6),    LCG(KC_7), LCG(KC_8),    LCG(KC_9), LCG(KC_0), /* |-------------| */  KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
+        _______,    KC_ESC, LSG(KC_LEFT),    KC_TAB, LSG(KC_RGHT),   QK_AREP, KC_MUTE /**/ KC_MUTE,  KC_PSLS,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, TG(4)),
+                                         TG(4), _______, _______, TG(4), _______, /* /----\ */ KC_PENT, KC_P0, KC_ESC, KC_NUM, TG(4)),
+
 
 // -----> Legacy;) layer <-----
-    [6] = LAYOUT(
-           KC_M,  KC_TAB, KC_Q, KC_W,    KC_E,    KC_R,   KC_T, /* |-----| */  KC_F5,   KC_1,  KC_3,   KC_5,  KC_NO, KC_CAPS, KC_TRNS,
-         KC_ESC, KC_RSFT, KC_A, KC_S,    KC_D,    KC_F,   KC_G, /* |-----| */  KC_F8,   KC_2,  KC_4,   KC_6,  KC_NO,   KC_NO, KC_TRNS,
-           KC_I, KC_LCTL, KC_Z, KC_X,    KC_C,    KC_V, /* ////---------------\\\\ */ QK_REP, KC_NO, KC_ENT, KC_ESC, KC_PSCR,   TG(6),
-                                      KC_RCTL, KC_LALT, KC_SPC, /* /-----\ */  TG(6), TG(6), TG(6))
+    [_LGCY] = LAYOUT(
+        KC_GRV,    KC_1,  KC_2,  KC_3,  KC_4,  KC_5, /* |-------------| */ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_M,    KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R, /* |-------------| */ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_ESC, KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F, /* |-------------| */ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        OS_I,   KC_LCTL,  KC_Z,  KC_X,  KC_C,  KC_V, KC_MUTE /**/ KC_MUTE,  QK_REP,   KC_NO,  KC_ENT,  KC_ESC, KC_PSCR,   TG(5),
+             XXXXXXX, XXXXXXX, KC_RCTL, KC_LALT, KC_SPC, /* /----\ */ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(5)),
+
 
 };
 
@@ -58,7 +71,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Encoders
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [_BASE] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_EXT]  = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [_SYM]  = { ENCODER_CCW_CW(LCS(KC_LEFT), LCS(KC_RGHT)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FUNC] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_WKNM] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_LGCY] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 #endif
 
